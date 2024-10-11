d68ba820407ee01e30032206b075e9d383d098dc
rafal-wit <turlejski.r@gmail.com>
2024-10-11 21:14:40 +0200

    init

--- temp/temp.tsx
+++ temp/temp.tsx
@@ -84,7 +84,7 @@ const getScoreColor = (score: any) => {
                   <div className="flex justify-between text-sm text-gray-500 mt-1">
                    <span>0</span>
                    <span>5</span></div>
-                   <div className="text-sm text-gray-500 mt-1">(Skala: 0 brak, 5 ocena najwyższa)
+                   <div className="text-sm text-gray-500 mt-1">( Skala: 0 brak, 5 ocena najwyższa )
                   </div>
 
                 </div>
